cda12452cc6806d5ef15446bc4be8baaf9e4005d
Matt Crump <crumpmj@gmail.com>
2019-09-03 15:06:29 -0400

    Update run.R

--- MinervaReasoning/run.R
+++ MinervaReasoning/run.R
@@ -1,4 +1,8 @@
 # Run all of this to run the experiment in a browser
+# critical randomization is done by R right now
+# so this should be run to initiate the experiment for each new subject
+# After the experiment is complete, the data should be stored in the data folder
+# when finished, "stop" the server that will be be started by this code
 
 library(xprmntr)
 
